625e4789ebfdb053ee0a48a0c23d9cce540bacfa
Kimle1812 <nguyenthikimle18122003@gmail.com>
2025-03-03 16:23:24 +0700

    homework8.js

--- Session19/homework8.js
+++ Session19/homework8.js
@@ -1,26 +1,14 @@
 let month=prompt("Mời bạn nhập số tháng");
-switch(month){
-    case "1":
-    case "2":
-    case "3":
+if(Number.isInterger(month)){
+    if(month<=3 && month >=1){
         console.log("mùa xuân");
-        break;
-    case "4":
-    case "5":
-    case "6":
+    }else if(month<=6 && month >=4){
         console.log("mùa hạ");
-        break;
-    case "7":
-    case "8":
-    case "9":
+    }else if(month<=9 && month >=7){
         console.log("mùa thu");
-        break;
-    case "10":
-    case "11":
-    case "12":
+    }else if(month<=12 && month >=10){
         console.log("mùa đông");
-        break;
-    default:
+    }else{
         console.log("tháng không hợp lệ");
-        break;
+    }
 }
