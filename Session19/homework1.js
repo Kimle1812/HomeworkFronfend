631c6428d95b7381531454ea5043ddd5a7dc568e
Kimle1812 <nguyenthikimle18122003@gmail.com>
2025-03-03 21:02:51 +0700

    homework1.js

--- Session19/homework1.js
+++ Session19/homework1.js
@@ -1,5 +1,5 @@
 let month=+prompt("Mời bạn nhập số tháng");
-if(month > 12 && month < 1){
+if(month <= 12 && month >= 1){
     if(month == 2){
         console.log("Tháng 2 có 28 hoặc 29 ngày năm nhuận");
     }else if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
